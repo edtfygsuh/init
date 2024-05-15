8a47afe80d1e8535eeeac6fed0053a5c892c2831
edtfygsuh <bsjcrandonbanos@hotmail.com>
2024-05-15 17:48:43 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function findTheDifference(s, t) {
-  let result = 0;
-  for (const char of s) {
-    result ^= char.charCodeAt(0);
-  }
-  for (const char of t) {
-    result ^= char.charCodeAt(0);
-  }
-  return String.fromCharCode(result);
-}
+const last = (arr) => arr[arr.length - 1];
